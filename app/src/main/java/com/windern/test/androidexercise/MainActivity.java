9710541573338756e2ec0c888140b1dfe513c74e
wenxinlin <wenxinlin@guazi.com>
2017-05-22 15:52:24 +0800

    引入butterknife的库

--- app/src/main/java/com/windern/test/androidexercise/MainActivity.java
+++ app/src/main/java/com/windern/test/androidexercise/MainActivity.java
@@ -2,12 +2,21 @@ package com.windern.test.androidexercise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.tv_test)
+    TextView mTvTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mTvTest.setText("2222");
     }
 }
